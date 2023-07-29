fa0c01203fb82e387e52aafd53b043dc59f289f4
Luis Fernando <luisfernandobr2003@gmail.com>
2023-07-29 13:44:24 -0300

    fix(Participant): fix styles code eslint

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -1,21 +1,17 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "./styles";
+import { Text, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
 
-type Props = { 
+type Props = {
   name: string
   onRemove: () => void
 }
 export function Participant({ name, onRemove }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>
-        {name}
-      </Text>
+      <Text style={styles.name}>{name}</Text>
 
       <TouchableOpacity style={styles.button} onPress={onRemove}>
-        <Text style={styles.buttonText}>
-          -
-        </Text>
+        <Text style={styles.buttonText}>-</Text>
       </TouchableOpacity>
     </View>
   )
--- src/components/Participant/styles.ts
+++ src/components/Participant/styles.ts
@@ -1,6 +1,6 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
-export const styles  = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     width: '100%',
     backgroundColor: '#1f1e25',
